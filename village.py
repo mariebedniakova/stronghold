6a0c7253c2db63e55293d171a315df50a235bcf3
mariebedniakova <maria.skif2547@gmail.com>
2021-01-18 21:14:02 +0300

    result 1.2

--- village.py
+++ village.py
@@ -117,7 +117,7 @@ def load_rules(filename):
 
 village = load_map('village_plan.txt')
 building_group = pygame.sprite.Group()
-resources = {'money': 500,
+resources = {'money': 100,
              'free_people': [],
              'warriors': [],
              'food': 0,
